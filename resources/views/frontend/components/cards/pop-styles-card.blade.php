29c434092d88065be9527be945c2c1863ea2c4f9
Denis Bashkirov <era-digital@yandex.ru>
2020-05-05 15:24:00 +0300

    commit

--- resources/views/frontend/components/cards/pop-styles-card.blade.php
+++ resources/views/frontend/components/cards/pop-styles-card.blade.php
@@ -1,5 +1,8 @@
 <div class="pop-styles-card swiper-slide">
-    <img src="{{ asset('img/design-styles/sample.jpg') }}">
+    <picture>
+        <source srcset="{{ asset('img/design-styles/sample.jpg') }}" type="image/webp">
+        <img src="{{ asset('img/design-styles/sample.jpg') }}" alt="">
+    </picture>
     <div class="pop-styles-card__overlay overlay xs-lg:hidden flex flex-center justify-center">
         <p class="text-white font-Montserrat font-bold xl:text-lg">Название стиля</p>
         <a class="button button-white-solid absolute bottom-4 text-sm" href="#">Подробнее</a>
