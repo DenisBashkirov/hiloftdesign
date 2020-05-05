e034553ae4148ec4c000828312e57c231d818139
Denis Bashkirov <era-digital@yandex.ru>
2020-05-05 15:39:27 +0300

    commit

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -5,7 +5,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @if(env('APP_ENV' == 'production'))
+    @if(env('APP_ENV') == 'production')
         <link rel="stylesheet" href="{{ asset('css/frontend.min.css') }}">
     @else
         <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
