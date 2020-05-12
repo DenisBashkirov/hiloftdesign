a0b006ad4d4baab1dda0050dcf31c88c933a72ae
Denis Bashkirov <era-digital@yandex.ru>
2020-05-12 20:27:29 +0300

    updted Portfolio style in breakpoing-lg

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -1,14 +1,14 @@
-<section class="title-screen bg-title-screen--home webp xs-sm:h-64 md:h-84 text-white xs-sm:pt-12 lg-xl:pt-20">
+<section class="title-screen bg-title-screen--home webp xs-sm:h-64 md-lg:h-84 text-white xs-sm:pt-12 xl:pt-20">
     <div class="overlay xl:hidden bg-black-100 xs-lg:op-50"></div>
     <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col justify-center xs-sm:flex-start xl:flex-start h-full">
 
-        <div class="xs-md:flex xs-md:flex-col xs-md:flex-start xs-md:sy-3">
-            <div class="breadcrumbs flex flex-nowrap flex-center xs-md:sx-2 font-light op-75">
+        <div class="xs-md:flex xs-md:flex-col xs-lg:flex-start xs-lg:sy-3">
+            <div class="breadcrumbs flex flex-nowrap flex-center xs-lg:sx-2 font-light op-75">
                 <a class="xs-sm:text-base" href="{{ route('home') }}"><i class="icon-home"></i></a>
                 <span class="breadcrumbs-arrow icon-arrow-right"></span>
                 <span>Портфолио</span>
             </div>
-            <h1 class="lg-xl:w-100 font-bold xs-sm:text-3xl md:text-5xl lg-xl:text-4xl xs-md:text-center">Портфолио</h1>
+            <h1 class="lg-xl:w-100 font-bold xs-sm:text-3xl md-xl:text-5xl xs-md:text-center">Портфолио</h1>
         </div>
 
     </div>
