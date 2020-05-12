14071bcb68dc3c0e0794155fbf96b6318a9464d8
Denis Bashkirov <era-digital@yandex.ru>
2020-05-12 19:51:50 +0300

    Update title-screen.blade.php

---
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -0,0 +1,15 @@
+<section class="title-screen bg-title-screen--home webp xs-sm:h-64 text-white xs-sm:pt-12 md-xl:pt-20">
+    <div class="overlay xl:hidden bg-black-100 xs-lg:op-50"></div>
+    <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col xs-md:justify-around lg-xl:justify-center xs-sm:flex-start xl:flex-start h-full">
+
+        <div class="xs-md:flex xs-md:flex-col xs-md:flex-center xs-sm:sy-3">
+            <div class="breadcrumbs flex flex-nowrap xs-sm:sx-2">
+                <a href="{{ route('home') }}"><i class="icon-home"></i></a>
+                <span class="icon-arrow"></span>
+                <span>Портфолио</span>
+            </div>
+            <h1 class="lg-xl:w-100 font-bold xs:text-xl sm:text-3xl md:text-5xl lg-xl:text-4xl xs-md:text-center">Портфолио</h1>
+        </div>
+
+    </div>
+</section>
