290eed31eb38516896d3494b87061bbba79dd442
Denis Bashkirov <era-digital@yandex.ru>
2020-05-12 20:24:40 +0300

    commit

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -1,9 +1,9 @@
-<section class="title-screen bg-title-screen--home webp xs-sm:h-64 text-white xs-sm:pt-12 md-xl:pt-20">
+<section class="title-screen bg-title-screen--home webp xs-sm:h-64 md:h-84 text-white xs-sm:pt-12 lg-xl:pt-20">
     <div class="overlay xl:hidden bg-black-100 xs-lg:op-50"></div>
-    <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col xs-md:justify-around lg-xl:justify-center xs-sm:flex-start xl:flex-start h-full">
+    <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col justify-center xs-sm:flex-start xl:flex-start h-full">
 
-        <div class="xs-md:flex xs-md:flex-col xs-sm:flex-start xs-sm:sy-3">
-            <div class="breadcrumbs flex flex-nowrap flex-center xs-sm:sx-2 xs-sm:text-sm op-75">
+        <div class="xs-md:flex xs-md:flex-col xs-md:flex-start xs-md:sy-3">
+            <div class="breadcrumbs flex flex-nowrap flex-center xs-md:sx-2 font-light op-75">
                 <a class="xs-sm:text-base" href="{{ route('home') }}"><i class="icon-home"></i></a>
                 <span class="breadcrumbs-arrow icon-arrow-right"></span>
                 <span>Портфолио</span>
