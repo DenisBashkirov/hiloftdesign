1b19b10c0fee246c961c70fdf2189f22d40aa22b
Denis Bashkirov <era-digital@yandex.ru>
2020-05-12 20:04:00 +0300

    commit

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -2,10 +2,10 @@
     <div class="overlay xl:hidden bg-black-100 xs-lg:op-50"></div>
     <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col xs-md:justify-around lg-xl:justify-center xs-sm:flex-start xl:flex-start h-full">
 
-        <div class="xs-md:flex xs-md:flex-col xs-md:flex-center xs-sm:sy-3">
-            <div class="breadcrumbs flex flex-nowrap xs-sm:sx-2">
-                <a href="{{ route('home') }}"><i class="icon-home"></i></a>
-                <span class="icon-arrow"></span>
+        <div class="xs-md:flex xs-md:flex-col xs-sm:flex-start xs-sm:sy-3">
+            <div class="breadcrumbs flex flex-nowrap xs-sm:sx-2 xs-sm:text-sm op-75">
+                <a class="xs-sm:text-base" href="{{ route('home') }}"><i class="icon-home"></i></a>
+                <span class="breadcrumbs-arrow icon-arrow"></span>
                 <span>Портфолио</span>
             </div>
             <h1 class="lg-xl:w-100 font-bold xs:text-xl sm:text-3xl md:text-5xl lg-xl:text-4xl xs-md:text-center">Портфолио</h1>
