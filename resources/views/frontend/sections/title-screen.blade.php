dbc82e93e91e2078978880e0d472b6b385fe91a2
Denis Bashkirov <era-digital@yandex.ru>
2020-05-12 20:59:09 +0300

    updated background sass

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -1,5 +1,5 @@
-<section class="title-screen bg-title-screen--home webp xs-sm:h-64 md-xl:h-84 xl:h-128 text-white xs-sm:pt-12">
-    <div class="overlay xl:hidden bg-black-100 xs-lg:op-50"></div>
+<section class="title-screen bg-title-screen--portfolio webp xs-sm:h-64 md-xl:h-84 xl:h-128 text-white xs-sm:pt-12">
+    <div class="overlay bg-black-100 op-50"></div>
     <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col justify-center xs-sm:flex-start xl:flex-start h-full">
 
         <div class="xs-md:flex xs-md:flex-col xs-lg:flex-start sy-3">
