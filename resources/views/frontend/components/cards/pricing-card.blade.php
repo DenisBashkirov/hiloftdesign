97593d49044be06a39b6b587e773ccb5a9762937
Denis Bashkirov <era-digital@yandex.ru>
2020-05-13 23:23:48 +0300

    pricing-card перемещён в отдельный компонент

---
+++ resources/views/frontend/components/cards/pricing-card.blade.php
@@ -0,0 +1,20 @@
+<div class="pricing-card flex flex-col flex-center py-10 xs-md:px-4 sy-6 text-white bg-grey-80 b-radius-5 swiper-slide">
+    <div class="text-center">
+        <div class="pricing-card__headline xs-sm:text-3xl md-xl:text-4xl font-Montserrat font-semibold">Базовый</div>
+        <div class="pricing-card__pricing mt-3">
+            <div class="pricing-card__price-value xs-sm:mt-1 font-bold xs-sm:text-3xl md-xl:text-4xl">1190 р</div>
+            <div class="pricing-card__unit font-bold md-xl:text-lg">за кв. метр</div>
+        </div>
+    </div>
+    <ol class="pricing-card__options-list xs-sm:text-xs sy-3">
+        <li>Обмеры</li>
+        <li>Планировочное решение</li>
+        <li>Строительные чертежи</li>
+        <li>3D визуализации</li>
+        <li>Подбор материалов</li>
+        <li>Ссылки на материалы</li>
+        <li>Проведение тендера</li>
+        <li>Авторский надзор</li>
+    </ol>
+    <div class="button button-white-solid xs-sm:w-full text-sm">Выбрать</div>
+</div>
