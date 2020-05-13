3f2f51d0285ad45886e22e5d2fedcb17702ebaa4
Denis Bashkirov <era-digital@yandex.ru>
2020-05-13 22:58:23 +0300

    Update services.blade.php

--- resources/views/frontend/pages/services/services.blade.php
+++ resources/views/frontend/pages/services/services.blade.php
@@ -17,10 +17,10 @@
                     @for ($i = 0; $i < 3; $i++)
                        <div class="pricing-card flex flex-col flex-center py-10 xs-md:px-4 sy-6 text-white bg-grey-80 b-radius-5 swiper-slide">
                            <div class="text-center">
-                               <div class="pricing-card__headline xs-sm:text-3xl md-lg:text-4xl font-Montserrat font-semibold">Базовый</div>
+                               <div class="pricing-card__headline xs-sm:text-3xl md-xl:text-4xl font-Montserrat font-semibold">Базовый</div>
                                <div class="pricing-card__pricing mt-3">
-                                   <div class="pricing-card__price-value xs-sm:mt-1 font-bold xs-sm:text-3xl md-lg:text-4xl">1190 р</div>
-                                   <div class="pricing-card__unit font-bold md:text-lg">за кв. метр</div>
+                                   <div class="pricing-card__price-value xs-sm:mt-1 font-bold xs-sm:text-3xl md-xl:text-4xl">1190 р</div>
+                                   <div class="pricing-card__unit font-bold md-xl:text-lg">за кв. метр</div>
                                </div>
                            </div>
                            <ol class="pricing-card__options-list xs-sm:text-xs sy-3">
