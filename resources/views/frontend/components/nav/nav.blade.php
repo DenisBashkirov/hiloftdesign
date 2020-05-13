875dbc7f39d0625a61589dc4d62efcfd2411f8dc
Denis Bashkirov <era-digital@yandex.ru>
2020-05-13 14:29:19 +0300

    commit

--- resources/views/frontend/components/nav/nav.blade.php
+++ resources/views/frontend/components/nav/nav.blade.php
@@ -1,7 +1,7 @@
 <a class="header-nav-item" href="{{ route('portfolio') }}">Портфолио</a>
 <span>
     <span>Услуги и цены</span>
-    <div class="header-nav-dropdown absolute flex flex-col sy-3 py-6 px-3 border-2 bg-grey-80">
+    <div class="header-nav-dropdown absolute flex flex-col left-0 sy-3 py-6 px-3 white-space-nowrap b-radius-2 bg-grey-80">
         <a class="header-nav-dropdown-item" href="#">Дизайн квартир</a>
         <a class="header-nav-dropdown-item" href="#">Дизайн домов и коттеджей</a>
     </div>
