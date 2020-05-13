a7df0d0c6940e7ae76d73eafcc61463f9502050c
Denis Bashkirov <era-digital@yandex.ru>
2020-05-13 22:53:10 +0300

    commit

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+    let breakpoints = {
+        xs: 0,
+        sm: 375,
+        md: 768,
+        lg: 1020,
+        xl: 1200
+    };
+
+    let mobile_xs = window.matchMedia('(max-width: ' + (breakpoints.sm - 1) + 'px)').matches;
+    let mobile_sm = window.matchMedia('(min-width: ' + (breakpoints.sm) + 'px) and (max-width: ' + (breakpoints.md - 1) + 'px)').matches;
+    let tablet_md = window.matchMedia('(min-width: ' + (breakpoints.md) + 'px) and (max-width: ' + (breakpoints.lg - 1) + 'px)').matches;
+    let tablet_lg = window.matchMedia('(min-width: ' + (breakpoints.lg) + 'px) and (max-width: ' + (breakpoints.xl - 1) + 'px)').matches;
+    let desktop = window.matchMedia('(min-width: ' + (breakpoints.xl) + 'px)').matches;
+
+
     let $headerDropdown = $('.js-header-nav-dropdown');
     $headerDropdown.on('mouseenter', function () {
         let $dropdownMenu = $(this).find('.header-nav-dropdown-wrapper');
@@ -126,7 +141,7 @@ $(document).ready(function () {
         }
     });
 
-    if($(window).width() >= 1200 && portfolioItemsSwiper.$wrapperEl !== undefined) {
+    if(desktop && portfolioItemsSwiper.$wrapperEl !== undefined) {
         portfolioItemsSwiper.destroy();
     }
 
@@ -178,5 +193,44 @@ $(document).ready(function () {
 
         }
     });
+
+
+
+    /*
+    pricing-cards-swiper
+     */
+    if (mobile_xs || mobile_sm || tablet_md || tablet_lg) {
+        let pricingCardsSwiper = new Swiper('.pricing-cards-swiper', {
+            spaceBetween: 30,
+            watchOverflow: true,
+            pagination: {
+                el: '.eshl-features-swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 'auto',
+                    centeredSlides: true,
+                    initialSlide: 1,
+                    spaceBetween: 15,
+                },
+                375: {
+                    slidesPerView: 'auto',
+                    centeredSlides: true,
+                    initialSlide: 1,
+                    spaceBetween: 30,
+                },
+                992: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+            }
+        });
+    }
 
 });
--- resources/views/frontend/pages/services/services.blade.php
+++ resources/views/frontend/pages/services/services.blade.php
@@ -5,4 +5,41 @@
     @component('frontend.sections.title-screen', ['page_h1'=>'Дизайн квартир', 'title_screen_bg'=>$service_name])
     @endcomponent
 
+    <section class="xs-sm:my-12 md:my-24 lg:my-28 xl:my-36">
+
+        <div class="container">
+            <h2 class="text-golden">Тарифы на услуги дизайна</h2>
+        </div>
+
+        <div class="lg-xl:container xs-sm:mt-6 md-lg:mt-12 xl:mt-20">
+            <div class="pricing-cards-swiper swiper-container">
+                <div class="swiper-wrapper">
+                    @for ($i = 0; $i < 3; $i++)
+                       <div class="pricing-card flex flex-col flex-center py-10 xs-md:px-4 sy-6 text-white bg-grey-80 b-radius-5 swiper-slide">
+                           <div class="text-center">
+                               <div class="pricing-card__headline xs-sm:text-3xl md-lg:text-4xl font-Montserrat font-semibold">Базовый</div>
+                               <div class="pricing-card__pricing mt-3">
+                                   <div class="pricing-card__price-value xs-sm:mt-1 font-bold xs-sm:text-3xl md-lg:text-4xl">1190 р</div>
+                                   <div class="pricing-card__unit font-bold md:text-lg">за кв. метр</div>
+                               </div>
+                           </div>
+                           <ol class="pricing-card__options-list xs-sm:text-xs sy-3">
+                               <li>Обмеры</li>
+                               <li>Планировочное решение</li>
+                               <li>Строительные чертежи</li>
+                               <li>3D визуализации</li>
+                               <li>Подбор материалов</li>
+                               <li>Ссылки на материалы</li>
+                               <li>Проведение тендера</li>
+                               <li>Авторский надзор</li>
+                           </ol>
+                           <div class="button button-white-solid xs-sm:w-full text-sm">Выбрать</div>
+                       </div>
+                    @endfor
+                </div>
+            </div>
+        </div>
+
+    </section>
+
 @endsection
