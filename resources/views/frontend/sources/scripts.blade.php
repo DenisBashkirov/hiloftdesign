cac6910157c29e512931e4397b3a155263506317
Denis Bashkirov <era-digital@yandex.ru>
2020-05-14 20:21:21 +0300

    Подключение внешнего скрипта карт перенесено вниз документа

--- resources/views/frontend/sources/scripts.blade.php
+++ resources/views/frontend/sources/scripts.blade.php
@@ -1,4 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://api-maps.yandex.ru/2.1/?apikey=ffd12f22-8ac8-4128-a78c-d675282765f0&lang=ru_RU"></script>
 <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
 <script src="{{ asset('libs/lazyload/lazysizes.min.js') }}" async=""></script>
 <script src="{{ asset('libs/modernizr/modernizr-custom.js') }}" async=""></script>
