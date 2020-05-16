299bffa0ba25230eaba8d398f4b8aa9bcc9d3ece
Denis Bashkirov <era-digital@yandex.ru>
2020-05-16 22:40:33 +0300

    Добавлены favicons

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -5,13 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @include('frontend.sources.favicons')
+
+    <meta property="og:type" content="product" />
+    <meta property="og:image" content="{{ asset('favicons/og-image.jpg') }}"/>
+    <meta property="og:title" content="Студия современного дизайна интерьеров | Hiloft Design Studio" />
+    <meta property="og:description" content="Профессиональный дизайн интерьеров. Работаем по Москве и регионам." />
+    <meta property="og:url" content="https://hiloftdesign.ru"/>
+    <meta property="og:site_name" content="Hiloft Design" />
+    <meta property="product:price:amount" content="" />
+    <meta property="product:price:currency" content="" />
+    <meta property="og:availability" content="instock" />
+
     <title>Document</title>
+
     @if(env('APP_ENV') == 'production')
         <link rel="stylesheet" href="{{ asset('css/frontend.min.css') }}">
     @else
         <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
     @endif
+
     @include('frontend.sources.styles')
+
 </head>
 <body>
 
