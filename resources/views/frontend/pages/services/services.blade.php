5cc5316a8fb0c9de181af3263b1bb13e19d9d59a
Denis Bashkirov <era-digital@yandex.ru>
2020-05-17 19:24:14 +0300

    commit

--- resources/views/frontend/pages/services/services.blade.php
+++ resources/views/frontend/pages/services/services.blade.php
@@ -60,7 +60,7 @@
 
     </section>
 
-    <section class="special-offers xs-sm:my-12 md:my-24 lg:my-28 xl:my-36">
+    <section class="special-offers xs-sm:my-12 md:my-24 lg:my-28 xl:my-36" id="special-offers">
 
         <div class="container">
             <h2 class="text-golden">Спецпредложения</h2>
--- resources/views/frontend/sections/service-cards.blade.php
+++ resources/views/frontend/sections/service-cards.blade.php
@@ -1,6 +1,6 @@
 <div class="container flex flex-wrap justify-between xs-sm:flex-col xs-sm:my-12 md:my-24 lg:my-28 xl:my-36 xs-sm:sy-8">
-    @component('frontend.components.cards.services-card', ['headline'=>'Дизайн квартир', 'background'=>'apartments'])
+    @component('frontend.components.cards.services-card', ['headline'=>'Дизайн квартир', 'service_name'=>'dizayn-kvartir','background'=>'apartments'])
     @endcomponent
-    @component('frontend.components.cards.services-card', ['headline'=>'Дизайн домов и коттеджей', 'background'=>'houses'])
+    @component('frontend.components.cards.services-card', ['headline'=>'Дизайн домов и коттеджей', 'service_name'=>'dizayn-domov','background'=>'houses'])
     @endcomponent
 </div>
