173ce959c9752aef24c667bb25db64ed8ac77705
Denis Bashkirov <era-digital@yandex.ru>
2020-05-17 22:02:20 +0300

    Update services-card.blade.php

--- resources/views/frontend/components/cards/services-card.blade.php
+++ resources/views/frontend/components/cards/services-card.blade.php
@@ -3,6 +3,6 @@
     <h3 class="xs-sm:text-3xl md-lg:text-3xl xl:text-4xl">{{ $headline }}</h3>
     <div class="flex flex-col xs-sm:w-56 md-xl:w-60 mx-auto xs-sm:mt-5 md-xl:mt-10 xs-sm:sy-4 md-xl:sy-8 text-sm">
         <a class="button button-white-ghost" href="{{ route('services', ['service'=>$service_name]) }}#special-offers">Спецпредложения</a>
-        <a class="button button-white-solid" href="{{ route('services', ['service'=>$service_name]) }}#special-offers">Стоимость дизайна</a>
+        <a class="button button-white-solid" href="{{ route('services', ['service'=>$service_name]) }}">Стоимость дизайна</a>
     </div>
 </div>
