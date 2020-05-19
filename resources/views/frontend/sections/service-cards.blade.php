e43623b61573041572332c4a89818a5bd0045f74
Denis Bashkirov <era-digital@yandex.ru>
2020-05-19 18:14:16 +0300

    Update service-cards.blade.php

--- resources/views/frontend/sections/service-cards.blade.php
+++ resources/views/frontend/sections/service-cards.blade.php
@@ -1,4 +1,4 @@
-<div class="container flex flex-wrap justify-between xs-sm:flex-col xs-sm:my-12 md:my-24 lg:my-28 xl:my-36 xs-sm:sy-8">
+<div class="container flex flex-wrap justify-between xs-sm:flex-col xs-sm:my-12 md:my-20 lg:my-24 xl:my-28 xs-sm:sy-8">
     @component('frontend.components.cards.services-card', ['headline'=>'Дизайн квартир', 'service_name'=>'dizayn-kvartir','background'=>'apartments'])
     @endcomponent
     @component('frontend.components.cards.services-card', ['headline'=>'Дизайн домов и коттеджей', 'service_name'=>'dizayn-domov','background'=>'houses'])
