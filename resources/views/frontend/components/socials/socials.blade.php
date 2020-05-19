f9b291aedcc95b9cf747175e14625ff081ca489d
Denis Bashkirov <era-digital@yandex.ru>
2020-05-19 19:17:52 +0300

    Update socials.blade.php

--- resources/views/frontend/components/socials/socials.blade.php
+++ resources/views/frontend/components/socials/socials.blade.php
@@ -1,9 +1,9 @@
 <span>
-    <a href="https://instagram.com/shepard_design/"><i class="icon-instagram"></i></a>
+    <a class="xl:hover-translate-up" href="https://instagram.com/shepard_design/"><i class="icon-instagram"></i></a>
 </span>
 <span>
-    <a href="https://vk.com/hiloftstudio"><i class="icon-vk-logo"></i></a>
+    <a class="xl:hover-translate-up" href="https://vk.com/hiloftstudio"><i class="icon-vk-logo"></i></a>
 </span>
 <span>
-    <a href="https://www.youtube.com/watch?v=M6sM-luBhmo"><i class="icon-youtube-icon"></i></a>
+    <a class="xl:hover-translate-up" href="https://www.youtube.com/watch?v=M6sM-luBhmo"><i class="icon-youtube-icon"></i></a>
 </span>
