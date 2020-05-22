967b00e4c772b1c79038db6d1bc95f1dc35a3b39
Denis Bashkirov <era-digital@yandex.ru>
2020-05-22 19:30:29 +0300

    Update title-screen.blade.php

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -1,6 +1,6 @@
 <section class="title-screen xs-sm:h-64 md-xl:h-84 xl:h-128 text-white xs-sm:pt-12">
     <div class="overlay bg-gradient-dark"></div>
-    <div class="overlay bg-title-screen--{{ $title_screen_bg }} webp op-40"></div>
+    <div class="overlay @isset($title_screen_bg) bg-title-screen--{{ $title_screen_bg }} @endisset webp op-40"></div>
     <div class="xs-lg:container xl:container-fluid xl:pl-8 relative flex flex-col justify-center xs-sm:flex-start xl:flex-start h-full">
 
         <div class="xs-md:flex xs-md:flex-col xs-lg:flex-start sy-3">
