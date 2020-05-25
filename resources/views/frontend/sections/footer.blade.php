a7b15641b3a89005e36515b89358795123ce0c26
Denis Bashkirov <era-digital@yandex.ru>
2020-05-25 19:57:11 +0300

    в footer добалены ссылки на Договор-оферту и Политику конфиденциальности

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer container-fluid as-end text-white bg-gradient-dark xs-sm:py-16 md:py-12 lg-xl:py-20">
     <div class="container">
-        <div class="flex xs-sm:flex-col md-xl:flex-row md-xl:justify-between md-xl:flex-center xs-sm:text-center">
+        <div class="flex xs-sm:flex-col md-xl:flex-row md-xl:justify-between md-xl:flex-center md:flex-wrap xs-sm:sy-8 xs-sm:text-center">
 
             <div>
                 <picture>
@@ -16,6 +16,11 @@
                     <img class="xs:w-60 sm:w-72 md-xl:w-40" src="{{ asset('img/logo/white_xs@2.png') }}" alt="Hiloft Design Studio логотип белый" />
                 </picture>
                 <p class="xs-sm:text-2xl md-xl:text-xl lh-100">Студия современного дизайна интерьера</p>
+            </div>
+
+            <div class="footer-links as-center xs-md:flex-order-3 flex xs-sm:flex-col md:flex-row lg-xl:flex-col xs-sm:flex-start md:mt-8 xs-sm:sy-2 md:sx-4 lg-xl:sy-2 xs-sm:text-left op-80">
+                <a class="underline-hover" href="{{ route('privacy_policy') }}">Политика конфиденциальности</a>
+                <a class="underline-hover" href="{{ route('contract_offer') }}">Договор-оферта</a>
             </div>
 
             <div class="xs-sm:mt-8 sx-6 xs-sm:text-3xl md-xl:text-3xl">
