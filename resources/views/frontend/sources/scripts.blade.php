c899b0942bac58f329a41f508ff54b8b010cac75
Denis Bashkirov <era-digital@yandex.ru>
2020-07-09 17:10:15 +0300

    Update scripts.blade.php

--- resources/views/frontend/sources/scripts.blade.php
+++ resources/views/frontend/sources/scripts.blade.php
@@ -1,6 +1,6 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="{{ asset('libs/jquery.easing/jquery.easing.1.3.min.js') }}"></script>
-<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script src="{{ asset('libs/lazyload/lazysizes.min.js') }}" ></script>
 <script src="{{ asset('libs/modernizr/modernizr-custom.js') }}" ></script>
 <script src="{{ asset('libs/fancybox/jquery.fancybox.min.js') }}" ></script>
