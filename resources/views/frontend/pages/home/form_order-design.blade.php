e77f9c06a40b76af9b2e37f21e01a839ffa46214
DenisBashkirov <bashkirov.denis@era-digital.ru>
2021-03-04 01:32:32 +0300

    Update form_order-design.blade.php

--- resources/views/frontend/pages/home/form_order-design.blade.php
+++ resources/views/frontend/pages/home/form_order-design.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid form_order-design webp">
     <div class="md-xl:container md-lg:py-20 xl:py-36">
         <div class="xs-sm:container md-lg:w-112 xl:w-136 md-xl:ml-auto py-16 bg-gradient-dark text-white text-center">
-            <h2 class="font-Montserrat font-semibold xs-sm:text-3xl md-lg:text-4xl xl:text-5xl">Заказать дизайн интерьера</h2>
+            <h2 class="font-Montserrat font-semibold xs-sm:text-3xl md-lg:text-4xl xl:text-5xl">Рассчитать дизайн&#8209;проект интерьера</h2>
             <form class="flex flex-col flex-center sy-12 mt-12" action="{{ route('thanks') }}" method="post">
                 @csrf
                 <input type="hidden" name="form_name" value="Заказать дизайн интерьера">
