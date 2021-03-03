3954c670e6ceb2c13f441064c116ea50e9cdac9a
DenisBashkirov <bashkirov.denis@era-digital.ru>
2021-03-04 01:43:23 +0300

    Update special-offer-card.blade.php

--- resources/views/frontend/components/cards/special-offer-card.blade.php
+++ resources/views/frontend/components/cards/special-offer-card.blade.php
@@ -1,6 +1,6 @@
 <div class="special-offer-card xl:w-cols-3 flex flex-col flex-center flex-center py-8 px-4 b-radius-5 overflow-hidden bg-white shadow swiper-slide">
     <div class="special-offer-title text-xl font-bold">{{ $title }}</div>
-    <ul class="special-offer-options-list sy-2 my-6 lh-125 text-sm op-70">
+    <ul class="special-offer-options-list sy-2 my-6 lh-125 text-sm op-70 sy-3">
         @foreach($options_list as $item)
             <li>{{ $item }}</li>
         @endforeach
