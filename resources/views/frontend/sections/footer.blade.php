608141fd070dd1f5038073ed80663c06c4db0cb5
DenisBashkirov <bashkirov.denis@era-digital.ru>
2021-03-04 19:42:23 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -4,16 +4,16 @@
 
             <div>
                 <picture>
-                    <source srcset="{{ asset('img/logo/white_xl@2.webp') }}" media="(min-width: 1200px)" type="image/webp">
-                    <source srcset="{{ asset('img/logo/white_xl@2.png') }}" media="(min-width: 1200px)">
-                    <source srcset="{{ asset('img/logo/white_lg@2.webp') }}" media="(min-width: 992px)" type="image/webp">
-                    <source srcset="{{ asset('img/logo/white_lg@2.png') }}" media="(min-width: 992px)">
-                    <source srcset="{{ asset('img/logo/white_md@2.webp') }}" media="(min-width: 768px)" type="image/webp">
-                    <source srcset="{{ asset('img/logo/white_md@2.png') }}" media="(min-width: 768px)">
-                    <source srcset="{{ asset('img/logo/white_sm@2.webp') }}" media="(min-width: 375px)" type="image/webp">
-                    <source srcset="{{ asset('img/logo/white_sm@2.png') }}" media="(min-width: 375px)">
-                    <source srcset="{{ asset('img/logo/white_xs@2.webp') }}" type="image/webp">
-                    <img class="xs:w-60 sm:w-72 md-xl:w-40" src="{{ asset('img/logo/white_xs@2.png') }}" alt="Hiloft Design Studio логотип белый" />
+                    <source srcset="{{ asset('img/logo/white-xl@2.webp') }}" media="(min-width: 1200px)" type="image/webp">
+                    <source srcset="{{ asset('img/logo/white-xl@2.png') }}" media="(min-width: 1200px)">
+                    <source srcset="{{ asset('img/logo/white-lg@2.webp') }}" media="(min-width: 992px)" type="image/webp">
+                    <source srcset="{{ asset('img/logo/white-lg@2.png') }}" media="(min-width: 992px)">
+                    <source srcset="{{ asset('img/logo/white-md@2.webp') }}" media="(min-width: 768px)" type="image/webp">
+                    <source srcset="{{ asset('img/logo/white-md@2.png') }}" media="(min-width: 768px)">
+                    <source srcset="{{ asset('img/logo/white-sm@2.webp') }}" media="(min-width: 375px)" type="image/webp">
+                    <source srcset="{{ asset('img/logo/white-sm@2.png') }}" media="(min-width: 375px)">
+                    <source srcset="{{ asset('img/logo/white-xs@2.webp') }}" type="image/webp">
+                    <img class="xs:w-60 sm:w-72 md-xl:w-40" src="{{ asset('img/logo/white-xs@2.png') }}" alt="Hiloft Design Studio логотип белый" />
                 </picture>
                 <p class="xs-sm:text-2xl md-xl:text-xl lh-100">Студия современного дизайна интерьера</p>
             </div>
