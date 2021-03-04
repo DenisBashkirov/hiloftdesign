9ebad7b9a4109a7fab8e75d3c492e2698b401ca1
DenisBashkirov <bashkirov.denis@era-digital.ru>
2021-03-04 19:37:43 +0300

    исправление отображения поп стилей

--- resources/views/frontend/components/cards/pop-styles-card.blade.php
+++ resources/views/frontend/components/cards/pop-styles-card.blade.php
@@ -1,15 +1,15 @@
 <div class="pop-styles-card swiper-slide">
     <picture>
 
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xl@1.webp') }}" media="(min-width: 768px)" type="image/webp">
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xl@1.jpg') }}" media="(min-width: 768px)">
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xl@2.webp') }}" media="(min-width: 768px) and (-webkit-min-device-pixel-ratio: 2)" type="image/webp">
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xl@2.jpg') }}" media="(min-width: 768px) and (-webkit-min-device-pixel-ratio: 2)">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xl@1.webp') }}" media="(min-width: 768px)" type="image/webp">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xl@1.jpg') }}" media="(min-width: 768px)">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xl@2.webp') }}" media="(min-width: 768px) and (-webkit-min-device-pixel-ratio: 2)" type="image/webp">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xl@2.jpg') }}" media="(min-width: 768px) and (-webkit-min-device-pixel-ratio: 2)">
 
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xs@2.webp') }}" media="(-webkit-min-device-pixel-ratio: 2)" type="image/webp">
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xs@1.webp') }}" media="(-webkit-device-pixel-ratio: 1)" type="image/webp">
-        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '_xs@2.jpg') }}" media="(-webkit-min-device-pixel-ratio: 2)">
-        <img class="lazyload" data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '__xs@1.jpg') }}" alt="">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xs@2.webp') }}" media="(-webkit-min-device-pixel-ratio: 2)" type="image/webp">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xs@1.webp') }}" media="(-webkit-device-pixel-ratio: 1)" type="image/webp">
+        <source data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xs@2.jpg') }}" media="(-webkit-min-device-pixel-ratio: 2)">
+        <img class="lazyload" data-srcset="{{ asset('img/design-styles/' . $style_name_slug . '/' . $style_name_slug . '-xs@1.jpg') }}" alt="">
 
     </picture>
     <div class="pop-styles-card__overlay overlay xs-lg:hidden flex flex-center justify-center">
